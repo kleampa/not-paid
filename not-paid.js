583d5907914c8de05606afda02f379ecf77c3a57
Ciprian Dragoi <ciprian@marketizator.com>
2016-10-06 00:27:51 +0300

    first version

---
+++ not-paid.js
@@ -0,0 +1,23 @@
+(function(){
+	/* change this variables as you wish */
+	var due_date = new Date('11/1/2016');
+	var days_deadline = 60;
+	/* stop changing here */
+	
+	var current_date = new Date();
+	var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
+	var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
+	var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
+	
+	if(days > 0) {
+		var days_late = days_deadline-days;
+		var opacity = (days_late*100/days_deadline)/100;
+			opacity = (opacity < 0) ? 0 : opacity;
+			opacity = (opacity > 1) ? 1 : opacity;
+		if(opacity >= 0 && opacity <= 1) {
+			document.getElementsByTagName("BODY")[0].style.opacity = opacity;
+		}
+		
+	}
+	
+})()
