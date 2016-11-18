a5383aa5284635f1e05daae0b602f152e85176ba
Eric Githinji <ericgithinjimaina@gmail.com>
2016-11-18 16:47:58 +0300

    Change due date
    So that it's easier to know where the `dd` and `mm` go in `mm/dd/yyyy`

--- not-paid.js
+++ not-paid.js
@@ -1,6 +1,6 @@
 (function(){
 	/* change these variables as you wish */
-	var due_date = new Date('11/1/2016');
+	var due_date = new Date('11/18/2016');
 	var days_deadline = 60;
 	/* stop changing here */
 	
