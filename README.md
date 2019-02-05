b7967e0696807666c2967ef0c0cce36d1687d03e
fl0at-on <megaw.mark@gmail.com>
2019-02-05 17:45:47 -0600

    Cleaning js code

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
-## Client did not pay?
+## Client hasn't paid?
 
-
-Add opacity to the body tag and increase it every day until their site completely fades away. Set a due date and customize the number of days you offer them until the website is fully vanished. 
-
+Reduce the opacity for the html body each day until their site completely fades away.
+Set the due date and number of days to extend until the website is fully vanished
 
 ```javascript
-/* change these variables as you wish */
-	var due_date = new Date('2017-02-27');
-	var days_deadline = 60;
-/* stop changing here */
+/* edit here */
+var dueDate = new Date("2019-01-1");
+var daysDeadline = 60;
+/* stop here */
 ```
 
 ## Usage
-Just load the .js file in ```<head>```
+
+Just load the .js file in `<body>`
 
 ## Author
 
 Inspired from twitter (@riklomas)
 Made by Ciprian, Romania
+Edited by fl0at-on
--- not-paid.js
+++ not-paid.js
@@ -1,23 +1,20 @@
-(function(){
-	/* change these variables as you wish */
-	var due_date = new Date('2017-02-27');
-	var days_deadline = 60;
-	/* stop changing here */
-	
-	var current_date = new Date();
-	var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
-	var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
-	var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
-	
-	if(days > 0) {
-		var days_late = days_deadline-days;
-		var opacity = (days_late*100/days_deadline)/100;
-			opacity = (opacity < 0) ? 0 : opacity;
-			opacity = (opacity > 1) ? 1 : opacity;
-		if(opacity >= 0 && opacity <= 1) {
-			document.getElementsByTagName("BODY")[0].style.opacity = opacity;
-		}
-		
-	}
-	
-})()
+(function() {
+  /* edit here */
+  const startDate = new Date("2019-01-1");
+  const daysExtended = 60;
+  /* stop here */
+
+  const currentDate = new Date();
+
+  //convert milliseconds to days
+  const daysOverdue = Math.floor(
+    (currentDate - startDate) / (1000 * 60 * 60 * 24)
+  );
+
+  if (daysOverdue > 0) {
+    const daysLate = daysExtended - daysOverdue;
+    let currentOpacity = daysLate / daysExtended;
+    currentOpacity = Math.max(0, Math.min(1, currentOpacity));
+    document.body.style.opacity = currentOpacity;
+  }
+})();
