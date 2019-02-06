edc22e595b54643ea80ce5242af2f11d13072a02
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-06 15:33:43 +0100

    added es6 features

--- not-paid.js
+++ not-paid.js
@@ -1,23 +1,23 @@
-(function(){
+(() => {
 	/* change these variables as you wish */
-	var due_date = new Date('2017-02-27');
-	var days_deadline = 60;
+	const due_date = new Date('2018-01-01');
+	const days_deadline = 60;
 	/* stop changing here */
-	
-	var current_date = new Date();
-	var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
-	var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
-	var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
-	
+
+	let current_date = new Date();
+	let utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
+	let utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
+	let days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
+
 	if(days > 0) {
-		var days_late = days_deadline-days;
-		var opacity = (days_late*100/days_deadline)/100;
+		let days_late = days_deadline-days;
+		let opacity = (days_late*100/days_deadline)/100;
 			opacity = (opacity < 0) ? 0 : opacity;
 			opacity = (opacity > 1) ? 1 : opacity;
 		if(opacity >= 0 && opacity <= 1) {
 			document.getElementsByTagName("BODY")[0].style.opacity = opacity;
 		}
-		
+
 	}
-	
+
 })()
