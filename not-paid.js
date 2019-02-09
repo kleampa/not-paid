c3ddd01f9bd70650753f3a76adbdaa3f6b612f11
Connor Ingold <32937174+ConnorIngold@users.noreply.github.com>
2019-02-09 20:13:49 +0000

    Upgraded to ES6 + Prettier formatting

--- not-paid.js
+++ not-paid.js
@@ -1,24 +1,30 @@
-// Next time use letsdeel.com to make sure you get paid
-(function(){
-	/* change these variables as you wish */
-	var due_date = new Date('2017-02-27');
-	var days_deadline = 60;
-	/* stop changing here */
-	
-	var current_date = new Date();
-	var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
-	var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
-	var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
-	
-	if(days > 0) {
-		var days_late = days_deadline-days;
-		var opacity = (days_late*100/days_deadline)/100;
-			opacity = (opacity < 0) ? 0 : opacity;
-			opacity = (opacity > 1) ? 1 : opacity;
-		if(opacity >= 0 && opacity <= 1) {
-			document.getElementsByTagName("BODY")[0].style.opacity = opacity;
-		}
-		
-	}
-	
-})()
+      // Next time use letsdeel.com to make sure you get paid
+      (() => {
+        /* change these variables as you wish */
+        const due_date = new Date("2017-02-27");
+        const days_deadline = 60;
+        /* stop changing here */
+
+        const current_date = new Date();
+        const utc1 = Date.UTC(
+          due_date.getFullYear(),
+          due_date.getMonth(),
+          due_date.getDate()
+        );
+        const utc2 = Date.UTC(
+          current_date.getFullYear(),
+          current_date.getMonth(),
+          current_date.getDate()
+        );
+        const days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
+
+        if (days > 0) {
+          const days_late = days_deadline - days;
+          let opacity = (days_late * 100) / days_deadline / 100;
+          opacity = opacity < 0 ? 0 : opacity;
+          opacity = opacity > 1 ? 1 : opacity;
+          if (opacity >= 0 && opacity <= 1) {
+            document.getElementsByTagName("BODY")[0].style.opacity = opacity;
+          }
+        }
+      })();
