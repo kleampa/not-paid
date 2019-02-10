4e7b0c6d9323a05aef08ff7c339b99fdad5592c5
Sam Holmes <samholmes1337@gmail.com>
2019-02-09 17:24:21 -0800

    Update not-paid.js
    Co-Authored-By: bsoyka <bensoyka@icloud.com>

--- not-paid.js
+++ not-paid.js
@@ -3,7 +3,7 @@
 	/* change these variables as you wish */
 	var due_date = new Date('2017-02-27');
 	var days_deadline = 60;
-    var deadline_message = ""; /* Leave empty string to disable */
+        var deadline_message = ""; /* Leave empty string to disable */
 	/* stop changing here */
 	
 	var current_date = new Date();
