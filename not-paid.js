c7daed52ef04212e81119bfe22a6a7b3ac045dea
whuang8 <willhuang08@gmail.com>
2019-02-09 22:22:33 -0600

    Tells user to give me your fkn money
    Redirects to popular "give me your fkn money" Vine video

--- not-paid.js
+++ not-paid.js
@@ -1,11 +1,20 @@
 // Next time use letsdeel.com to make sure you get paid
 (function(){
+	var give_me_your_fkn_money_url = "https://www.youtube.com/watch?v=HFmUAVD4iks"
+
 	/* change these variables as you wish */
 	var due_date = new Date('2017-02-27');
 	var days_deadline = 60;
+	var give_me_your_fkn_money = true
 	/* stop changing here */
 	
 	var current_date = new Date();
+
+	if current_date > due_date {
+		window.location = give_me_your_fkn_money_url
+		return
+	}
+
 	var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
 	var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
 	var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
